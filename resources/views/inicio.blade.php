01327aa04178dd5fcc689502813800ebd0906b84
Melvin Alfaro <melvii22n@gmail.com>
2023-03-05 13:07:34 -0600

    Se agregaron las cartas

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/cartas.css') }}">
+    <title>Sembrador Escolar</title>
 </head>
 
 <body>
@@ -15,7 +16,7 @@
 
         <div class="main">
             <x-appbar />
-            {{-- Cartas --}}
+            <x-cartas />
         </div>
     </div>
 
